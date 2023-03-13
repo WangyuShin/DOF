ab7f37f777e2cb908a8f6be3767071f73e305169
shin <syyhuk@gmail.com>
2023-03-13 18:58:17 +0900

    url_for import 추가

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, send_file, url_for
 
 from pymongo import MongoClient
 client = MongoClient("mongodb+srv://kmanpysev20:0000@cluster0.cknzfkt.mongodb.net/?retryWrites=true&w=majority")
